cfd8e2e41d0c270aa87e3f80d5b56c122b464767
men21049 <men21049@byui.edu>
2025-08-04 17:16:48 -0600

    first commit

---
+++ UniversePic/src/js/main.js
@@ -0,0 +1,8 @@
+import { loadHeaderFooter } from "./utils.mjs";
+import "uikit/dist/css/uikit.min.css";
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
+
+UIkit.use(Icons);
+
+loadHeaderFooter();
---
+++ UniversePic/src/js/utils.mjs
@@ -0,0 +1,37 @@
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  const htmlStrings = list.map(templateFn);
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement?.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+export function renderWithTemplate(templateFn, parentElement, data, callback) {
+  parentElement.innerHTML = templateFn;
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../public/partials/header.html");
+  const headerElement = document.querySelector("#main-header");
+
+  const footerTemplate = await loadTemplate("../public/partials/footer.html");
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}
